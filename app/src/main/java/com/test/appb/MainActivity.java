4ec0fecc13f24dd130b94bd28ca7799f27d4f3ed
allen <973395906@qq.com>
2019-03-06 19:57:52 +0800

    2、改包名dev_1.2.0

--- app/src/main/java/com/test/appb/MainActivity.java
+++ app/src/main/java/com/test/appb/MainActivity.java
@@ -3,6 +3,8 @@ package com.test.appb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.test.app.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
