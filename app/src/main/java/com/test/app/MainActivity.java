c6ac41ec3fd568e07c61958ae86dbf7a771fa365
allen <973395906@qq.com>
2019-03-06 20:42:37 +0800

    1.修改1.2.3分支的包名

--- app/src/main/java/com/test/app/MainActivity.java
+++ app/src/main/java/com/test/app/MainActivity.java
@@ -9,14 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //         111
-        //         222
-        //         333
-        //   lallallallalla
-//yyyyyyyyyyyyyy
-
-//        kkkk
-        //master 分支
-        //dev_1.2.0最新修改
+        //marst最新
     }
 }
