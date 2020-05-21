68caa16b2b16268a93ba1cd3b0ba3287be8b343f
allen <973395906@qq.com>
2020-05-21 19:18:49 +0800

    2、合并分支

--- app/src/main/java/com/test/app/MainActivity.java
+++ app/src/main/java/com/test/app/MainActivity.java
@@ -1,14 +1,14 @@
 package com.test.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //marst最新
+        //marst最新  2020
     }
 }
