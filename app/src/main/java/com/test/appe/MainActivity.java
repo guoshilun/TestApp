af223bbb0ad69c18973ad3f1a302df91bca6893f
allen <973395906@qq.com>
2021-06-09 19:59:29 +0800

    [fix] 点击事件

--- app/src/main/java/com/test/appe/MainActivity.java
+++ app/src/main/java/com/test/appe/MainActivity.java
@@ -2,28 +2,12 @@ package com.test.appe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-
-import com.google.gson.Gson;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    ArrayList<String> list = new ArrayList<>();
-    String[] array = new String[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.e("tag", list.toString());
-                Log.e("tag", new Gson().toJson(list));
-            }
-        });
     }
 }
