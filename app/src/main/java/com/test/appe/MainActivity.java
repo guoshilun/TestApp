6ea55f1682c3d42f45a3f8f38a0e01b6592f5e6a
allen <973395906@qq.com>
2021-06-09 20:06:00 +0800

    [fix] 多次提交2

--- app/src/main/java/com/test/appe/MainActivity.java
+++ app/src/main/java/com/test/appe/MainActivity.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    ///合并多次提交
+    ///合并多次提交1
+    ///合并多次提交2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
