2636b43f4328017a4a9a530b936c13af0f4160a2
allen <973395906@qq.com>
2021-06-09 19:48:01 +0800

    [fix] 删除注释

--- app/src/main/java/com/test/appe/MainActivity.java
+++ app/src/main/java/com/test/appe/MainActivity.java
@@ -18,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        Log.e("tag", "测试 proposed分支");
         findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
