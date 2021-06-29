52493cd5e78a37fb61ff05a232b4303d7dbd7200
allen <973395906@qq.com>
2021-06-29 09:03:40 +0800

    [fix] 合并提交
    【fix】6
    
    【fix】修改米趣家为米趣
    
    【fix】解决冲突

--- app/src/main/java/com/test/appe/activity/MainActivity.kt
+++ app/src/main/java/com/test/appe/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.test.appe
+package com.test.appe.activity
 
 import android.content.Context
 import android.content.Intent
@@ -6,8 +6,8 @@ import android.os.Bundle
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.RadioGroup
+import com.test.appe.R
 import com.test.appe.fragment.HomeFragment
 import com.test.appe.fragment.MineFragment
 import com.test.appe.fragment.TypeFragment
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private var mf: MineFragment? = null
     private var mATMessages = mutableListOf<String>()
     private var list = mutableListOf<String>()
+
     companion object {
         fun launchActivity(context: Context) {
             val intent = Intent(context, MainActivity::class.java)
@@ -45,9 +46,9 @@ class MainActivity : AppCompatActivity() {
             R.id.rb_home -> if (hf == null) {
                 hf = HomeFragment.newInstance()
                 ft.add(R.id.home_fragment, hf)
-                tv_title.text = "米趣家"
+                tv_title.text = "米趣"
             } else {
-                tv_title.text = "米趣家"
+                tv_title.text = "米趣"
                 ft.show(hf)
             }
             R.id.rb_type -> if (tf == null) {
--- app/src/main/java/com/test/appe/activity/SplashActivity.kt
+++ app/src/main/java/com/test/appe/activity/SplashActivity.kt
@@ -1,8 +1,9 @@
-package com.test.appe
+package com.test.appe.activity
 
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import com.test.appe.R
 
 class SplashActivity : AppCompatActivity() {
     private var mainHandler = Handler()
@@ -16,6 +17,6 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun enterApp() {
-        MainActivity.launchActivity(this)
+        EmojiActivity.launchActivity(this)
     }
 }
